c7009bca186296bb7d8f23739af7cd1a0d2c3833
NoMcGowan <113493454+NoMcGowan@users.noreply.github.com>
2022-10-07 00:11:17 -0500

    Add files via upload

--- Task3.java
+++ Task3.java
@@ -1,25 +1,70 @@
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.*;
+
 public class Task3
 {
 
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in);
-		String sen = sc.nextLine();
 
-		int i = 0;
-		boolean cap = true;
-		StringBuilder sb = new StringBuilder(sen);
-		while (i < sb.length()) {
-		    if (sb.charAt(i) == '.') {
-		        cap = true;
-		    } else if (cap && !Character.isWhitespace(sb.charAt(i))) {
-		        sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
-		        cap = false;
-		    }
-		    i++;
+		Scanner sc = new Scanner(System.in);
+		List<Boolean> list = new ArrayList<>();
+
+		while (sc.hasNextLine())
+		{
+			String b = sc.nextLine();
+			if (!b.isEmpty())
+			{
+				list.add(isBalanced(b));
+			} else
+			{
+				sc.close();
+				break;
+			}
 		}
-		System.out.println(sb.toString());
+
+		for (int n = 0; n < list.size(); n++)
+		{
+			System.out.println(list.get(n) + " ");
+		}
+
 	}
 
+	private static boolean isBalanced(String b)
+	{
+		boolean bal = false;
+		Stack<Character> stack = new Stack();
+		int num1 = 0;
+		int num2 = 0;
+		for (int i = 0; i < b.length(); i++)
+		{
+
+			if (b.charAt(i) == '{' || b.charAt(i) == '(' || b.charAt(i) == '[')
+			{
+
+				stack.push(b.charAt(i));
+				num1++;
+			}
+
+			if (b.charAt(i) == '}' || b.charAt(i) == ')' || b.charAt(i) == ']')
+			{
+
+				stack.push(b.charAt(i));
+				num2++;
+			}
+
+			if (stack.firstElement() == '}' || stack.firstElement() == ')' || stack.firstElement() == ']')
+			{
+				num2++;
+			}
+
+		}
+		if (num1 == num2)
+		{
+			return true;
+		}
+		return false;
+
+	}
 }
